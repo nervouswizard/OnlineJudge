5faf4ee2b176eb3751856a359224cfc6537e8008
nervouswizard <nervouswizard@users.noreply.github.com>
2019-08-01 23:01:34 +0800

    Update c124. 00532 - Dungeon Master.cpp

--- c124. 00532 - Dungeon Master.cpp
+++ c124. 00532 - Dungeon Master.cpp
@@ -1,49 +1,75 @@
-#include<iostream>
-#include<string>
-#include<queue>
-#include<cstring>
+#include<bits/stdc++.h>
+#define jizz ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define MP make_pair
+#define pb push_back
+#define F first
+#define S second
 using namespace std;
-int dx[6]={-1,1,0,0,0,0}, dy[6]={0,0,1,-1,0,0}, dz[6]={0,0,0,0,1,-1};
-int d[35][35][35], L, c, r, foo=0, Si, Sj, Sk, Ei, Ej, Ek;
-struct Av{
-	int x, y, z, f;
+typedef pair<int,int> pii;
+struct T
+{
+	int l, r, c, f;
 };
-void bfs(){
+int dx[6]={-1,1,0,0,0,0}, dy[6]={0,0,1,-1,0,0}, dz[6]={0,0,0,0,1,-1};
+int l, r, c, ans;
+int d[35][35][35];
+char ch;
+T B, E;
+void build(T d1, int d2, int d3, int d4)
+{
+	d1.l=d2, d1.r=d3, d1.c=d4;
+}
+int check(int d1, int d2, int d3)
+{
+	if (d1>0&&d2>0&&d3>0&&d1<=l&&d2<=r&&d3<=c) return 1;
+	return 0;
+}
+void bfs()
+{
 	int f;
-	queue<Av>q; q.push(Av{Si, Sj, Sk, 0}); 
-	d[Si][Sj][Sk]=1; 
-	foo=0;
-	while (!q.empty()){
-		Av v=q.front();q.pop();f=v.f;
-		for (int i=0;i<6;++i){
-			if (!d[v.x+dx[i]][v.y+dy[i]][v.z+dz[i]]){
-				q.push(Av{v.x+dx[i], v.y+dy[i], v.z+dz[i], f+1});
-				d[v.x+dx[i]][v.y+dy[i]][v.z+dz[i]]=1;
+	queue<T> q; q.push(B);
+	while (!q.empty())
+	{
+		T n=q.front(); q.pop();
+		f=n.f;
+		for (int i=0;i<6;i++)
+		{
+			T v={n.l+dx[i], n.r+dy[i], n.c+dz[i], f+1};
+			if (d[v.l][v.r][v.c]==0)
+			{
+				q.push(v);
+				d[v.l][v.r][v.c]=1;
 			}
-			if (v.x+dx[i]==Ei&&v.y+dy[i]==Ej&&v.z+dz[i]==Ek) {foo=f; break;}
+			if (v.l==E.l&&v.r==E.r&&v.c==E.c)
+				ans=v.f;
 		}
 	}
-	while (!q.empty()) q.pop();
 }
-int main(){
-	ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-	char ch;
-	memset(d,0,sizeof(d));
-	while(cin >> L >> r >> c, L+r+c){
-		for (int i=0;i<=L+1;++i){					
-			for (int j=0;j<=r+1;++j)				
-				for (int k=0;k<=c+1;++k){			
-					if(i>0&&j>0&&k>0&&i<=L&&j<=r&&k<=c)	{
+int main()
+{jizz
+	while (cin >> l >> r >> c, l+r+c)
+	{
+		ans=0;
+		for (int i=0;i<=l+1;i++)
+		{
+			for (int j=0;j<=r+1;j++)
+			{
+				for (int k=0;k<=c+1;k++)
+				{
+					if (check(i, j, k))
+					{
 						cin >> ch;
-						if (ch=='S')	d[i][j][k]=0, Si=i, Sj=j, Sk=k;
-						else if (ch=='#')	d[i][j][k]=1;
-						else if (ch=='.')	d[i][j][k]=0;
-						else d[i][j][k]=0, Ei=i, Ej=j, Ek=k;
+						if (ch=='S') B=T{i, j, k, 0}, d[i][j][k]=1;
+						else if (ch=='E') E=T{i, j, k, 0}, d[i][j][k]=0;
+						else if (ch=='.') d[i][j][k]=0;
+						else d[i][j][k]=1;
 					}
 					else d[i][j][k]=1;
 				}
+			}
 		}
 		bfs();
-		cout << (foo!=0?"Escaped in "+to_string(foo+1)+" minute(s).\n":"Trapped!\n");
+		if (ans==0) cout << "Trapped!\n";
+		else cout << "Escaped in " << ans << " minute(s).\n";
 	}
-}
+} 
